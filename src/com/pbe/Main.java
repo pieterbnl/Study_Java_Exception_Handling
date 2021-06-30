09c288b9d8d07bc59e331f32beba5343daba0d7e
Pieter Beernink <60547491+pieterbnl@users.noreply.github.com>
2021-06-30 20:17:27 +0200

    add multiple catch statements example

--- src/com/pbe/Main.java
+++ src/com/pbe/Main.java
@@ -60,8 +60,10 @@ public class Main {
 
         // Example of a divide by zero error, not being handled
         System.out.println("Division by zero error, without own exception handling");
+        System.out.println("-------------------------------------------");
 //        int a = 0;
 //        int b = 10 / 0; // will cause a 'ArithmeticException: / by zero' error
+        System.out.println();
 
         // Example of a divide by zero error, with try & catch
         // Try and catch form a unit. Once an exception is thrown, the program control transfers out of the try block into the catch block.
@@ -69,8 +71,9 @@ public class Main {
         // Goal of a try-catch is to resolve an exceptional condition and continue on as if the error never happened.
         // Exceptions are returned with a string that contains a description of the exception, that can be displayed.
         // Or an own exception description can be displayed.
+        // NOTE: variables introduced in the try block, only exist within that block
         System.out.println("Division by zero error, with try and catch");
-        // Now in try block
+        System.out.println("-------------------------------------------");
         try {
             int a = 0;
             int b = 10 / 0; // will cause a 'ArithmeticException: / by zero' error
@@ -79,7 +82,47 @@ public class Main {
             System.out.println("Exception: " + e); // this will display the string containing a description of the exception
             System.out.println("Division by zero."); // alternatively: use an own description
         }
-        System.out.println("After catch. \n");
+        System.out.println("After try/catch \n");
+
+        // Example of multiple catch clauses
+        // In some cases, it's useful to be able to catch multiple exceptions
+        // For this purpose, two or more catch clauses can be used, each catching a different exception
+        // With an exception thrown, each catch statement is inspected one after another
+        // The catch statement which type matches first, will be executed, bypassing the others
+        // Execution then continues after the try / catch block
+        System.out.println("Multiple catch clauses");
+        System.out.println("-------------------------------------------");
+        try {
+            int a = args.length; // will result in a = 0 if no command line arguments are provided at program start
+            int b = 10 / a; // causing a '/ zero' exception
+            int c[] = {1, 2, 3};
+            c[10] = 11; // causing an 'out of bounds' exception as c[] consists of only 3 elements
+        } catch (ArithmeticException e) {
+            System.out.println("Divide by 0: " + e);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index oob: " + e); // program will only get to this point if args are provided (read: if a > 0)
+        }
+        System.out.println("After try/catch blocks \n");
+
+
+        // With multiple catch statements, subclasses must come before any of their superclasses.
+        // Because a catch statement that uses a superclass, will catch exceptions of that type PLUS any of its subclasses.
+        // A subclass would never be reached if it came after its superclass.
+        // Note: in Java, unreachable code is an error
+        System.out.println("Multiple catch clauses: subclass must come before its superclass");
+        System.out.println("-------------------------------------------");
+        try {
+            int a = 0;
+            int b = 1 / 0;
+        } catch (Exception e) {
+            System.out.println("Generic Exception catch \n");
+        }
+//        catch (ArithmeticException e) {
+//            As ArithmeticException is a subclass of exception, this catch line is unreachable and will be causing an error
+//            This is because the general exception catch has already caught the exception.
+//            This problem can be fixed by switching the catch statements, with the ArithmeticException catch coming first.
+//            System.out.println("This line will never be reached.");
+//        }
 
     }
 }
